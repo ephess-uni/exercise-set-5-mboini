ff473a712c95bac29e9f768fbb7cc5c2ba4c92d4
mboini <123262514+mboini@users.noreply.github.com>
2023-03-03 08:02:12 +0530

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -7,6 +7,7 @@ This module contains an entry point that
 - writes the processed data to a file called `ex_5_2-processed.csv`
 """
 import numpy as np
+import os
 
 try:
     from src.util import get_repository_root
@@ -15,12 +16,24 @@ except ImportError:
 
 
 if __name__ == "__main__":
-
+    
     # Use these predefined input / output files
+
     root_dir = get_repository_root()
+
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
 
+    data = np.loadtxt(INFILE)
+    pre = (data - data.mean(axis=0)) / data.std(axis=0)
+    processed = pre
+    
+    
+
     # Complete the data processing steps using numpy here.
 
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+
     # Save the output to OUTFILE using numpy routines.
+
+    np.savetxt(OUTFILE, processed, fmt='%.2e')
