3ed245c2bd6b1e67e48c8d692368a11e2ce367c8
mboini <123262514+mboini@users.noreply.github.com>
2023-03-03 08:30:52 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -11,21 +11,23 @@ except ImportError:
 if __name__ == "__main__":
     # Create the argument parser object
     parser = ArgumentParser(description='Process input file of data, scale and shift to mean 0 and standard deviation 1, and save to output file')
-    parser.add_argument('input_file', help='path to input file of data')
-    parser.add_argument('output_file', help='path to output file')
+    parser.add_argument('infile', help='path to input file of data',nargs='?')
+    parser.add_argument('outfile', help='path to output file'nargs='?')
 
     # Parse the input and output file arguments from the command line
     args = parser.parse_args()
 
     # Load the input data from the input file
-    input_data = np.loadtxt(args.input_file)
+    input_data = np.loadtxt(args.infile)
 
     # Scale and shift the data to have mean 0 and standard deviation 1
-    scaled_data = (input_data - input_data.mean(axis=0)) / input_data.std(axis=0)
+    scaled_data1 = (input_data - input_data.mean(axis=0))
+    scaled_data2 = input_data.std(axis=0)
+    result = scaled_data1 / scaled_data2
 
     # Create the output directory if it does not already exist
     root_dir = get_repository_root()
     os.makedirs(root_dir / "outputs", exist_ok=True)
 
     # Save the processed data to the output file
-    np.savetxt(args.output_file, scaled_data, fmt='%.2e')
+    np.savetxt(args.outfile, result, fmt='%.2e')
